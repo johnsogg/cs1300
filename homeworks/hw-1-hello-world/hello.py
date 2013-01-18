6f223ab2ad328ac2403465a318283891bd0fe4e1
knaufr <jamesk2846@gmail.com>
2013-01-17 23:02:58 -0700

    Update homeworks/hw-1-hello-world/hello.py

--- homeworks/hw-1-hello-world/hello.py
+++ homeworks/hw-1-hello-world/hello.py
@@ -3,8 +3,10 @@
 #
 # hello.py
 #
-# Student name: 
-# Your email address:
+# Student name: Ryan Knauf  
+# Your email address: jamesk2846@gmail.com
 
 # write your program below here. You may have it print whatever text
 # you like.
+
+print "Hello, World!"
