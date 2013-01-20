d8ea530fe35222250c57b3868ced5c8e15b07842
Gabe Johnson <gabe.johnson@gmail.com>
2013-01-20 13:33:10 -0700

    Adding updates file, and the script used in class, and the intro videos from Andrew.

---
+++ recitations/cs1300.py
@@ -0,0 +1,17 @@
+print "Hello 1300"
+print 'Hello there guys how are y\'all?'
+foo = 2*3-2/2
+bar = ((2 * 7) - (2 / 2))
+print foo
+print bar
+my_dog = "Sputnik"
+print my_dog
+print my_dog * 2
+dot = "."
+print dot * 10
+foobar = foo * bar
+print foobar
+print "this is a divider"
+print foobar * dot
+# print "There are " + foo + " foos in here."
+print "There are " + str(foo) + " foos in here."
