0c4b4d2c876c1387ffec6d94ac50544772dfd998
Gabe Johnson <gabe.johnson@gmail.com>
2013-04-05 14:53:38 -0600

    Updates from class (updated lec notes too)

--- homeworks/hw-7-cpp-basics/cpp/cpp_basics.cpp
+++ homeworks/hw-7-cpp-basics/cpp/cpp_basics.cpp
@@ -18,6 +18,7 @@
 */
 
 #include <iostream>
+#include <cmath>
 #include "cpp_basics.h"
 
 using namespace std;
@@ -31,14 +32,63 @@ float get_percent(int score, int maxpoints) {
 }
 
 int strangeness(int x, int y) {
-  return 0;
+  //   increment = -10
+  int increment = -10;
+  //   count = 0
+  int count = 0;
+  //   while x > y:
+  while (x > y) {
+  //       x = x + increment
+    x = x + increment;
+  //       count = count + 1
+    count = count + 1;
+  }
+  //   increment = -1
+  increment = -1;
+  //   while y > x:
+  while (y > x) {
+  //       y = y + increment
+    y = y + increment;
+  //       count = count + 1
+    count = count + 1;
+  }
+  //   return count
+  return count;
 }
 
 int get_size_for_n_doubles(int n) {
-  return 0;
+  if (n < 0) {
+    return 0;
+  }
+  return sizeof(double) * n;
 }
 
+/**
+ * Determine if a number is periwinkle or not.
+ *
+ * A number is periwinkle if any of the following are true:
+ *
+ * - it is between 0 and 10 inclusive.
+ * - it is evenly divisible by 2, 3, 7, 13, or 19
+ * - it is less than -400
+ *
+ * If none of the above periwinkle rules match, n is not periwinkle.
+ *
+ * 1 point (requires is_gamboge to work as well)
+ **/
 bool is_periwinkle(int n) {
+  if ((0 <= n) && (n <= 10)) {
+    return true;
+  }
+  if ((n % 2 == 0) ||
+      (n % 3 == 0) ||
+      (n % 7 == 0)) {
+    // bug! The above list is not complete :)
+    return true;
+  }
+  if (n < -400) {
+    return true;
+  }
   return false;
 }
 
